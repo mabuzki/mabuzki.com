6ee3f7c8f169e06121e11f928beff3c075d00bdf
mabuzki <17908574+mabuzki@users.noreply.github.com>
2019-03-28 23:45:01 +0800

    Update changelog.vue

--- src/components/page/changelog.vue
+++ src/components/page/changelog.vue
@@ -1,5 +1,5 @@
 <template>
-<main>
+<main class="page-changelog">
 	<section class="section">
 		<div class="container">
 			<h2 class="title is-3">
@@ -20,22 +20,24 @@
 			<div class="content">
 				<div class="field">
 					<h4 class="title">
-						2019/3/28
+						2019/3/28<em class="title is-6">0.2.0</em>
 					</h4>
 					<ul>
 						<li>前端: Vuex针对令牌优化</li>
 						<li>前端: 注册/登录流程优化</li>
 						<li>前端: 用户页不同用户切换逻辑bug修复</li>
 						<li>前端: 用户页权限优化，设置页面布局优化</li>
-						<li>后端: 部分controler auth判断调整</li>
+						<li>前端: Toast现在集中发放，单独post不再接管此接口</li>
+						<li>后端: auth权限判断调整</li>
 						<li>后端: auth key重新签发功能优化</li>
 					</ul>
 				</div>
 				<div class="field">
 					<h4 class="title">
-						2019/3/14 
+						2019/3/14<em class="title is-6">0.1.0</em>
 					</h4>
 					<ul>
+
 						<li>Alpha online</li>
 					</ul>
 				</div>
@@ -49,3 +51,11 @@
 export default {
 }
 </script>
+
+<style>
+em.title {
+	margin-left: 10px;
+	font-style: normal
+}
+</style>
+
